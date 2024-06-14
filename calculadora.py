b58a0b66097220a7ec68d86588256bb63a02fbff
Instrutor <gilberto.braga0021@gmail.com>
2024-06-14 09:16:16 -0300

    - Commit inicial.

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+# Calculadora simples
+
+while True:
+
+    print('\n\t\t\t -- Calculadora Simples --\n')
+
+    print('1. soma')
+    print('2. sair')
+
+    op = int(input('\n\t Opção: '))
+
+    if op == 1:
+        print('\n\t\t\t -- Soma -- \n')
+        # Entradas
+
+        # Processamento
+
+        # Saída
+
+    elif op == 2:
+        print('\nForte abraço!\n')
+        break
+    else:
+        print('Opção {} incorreta'.format(op))
